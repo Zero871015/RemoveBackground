b76501396744f63017dbbb15fbc31f05a7318218
Zero871015 <35004099+Zero871015@users.noreply.github.com>
2018-12-27 16:42:01 +0800

    Add files via upload

---
+++ RemoveBackground/RemoveBackground/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace RemoveBackground
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "選擇圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(61, 25);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "10";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(102, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(195, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "去除顏色";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(285, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "改變背景顏色";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(402, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "輸出圖片";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(539, 377);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ RemoveBackground/RemoveBackground/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoveBackground
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            button2.Enabled = true;
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Bitmap image = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = image;
+            pictureBox1.Size = image.Size;
+            this.Height = pictureBox1.Size.Height+200;
+            this.Width = pictureBox1.Size.Width + 200;
+        }
+
+
+       
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Bitmap clickcolor;
+            Color clickcolor;
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            Bitmap newimage = new Bitmap(pictureBox1.Image);
+            clickcolor = image.GetPixel(e.X,e.Y);
+            for (int i = 0; i < 20; i++)
+                for (int j = 0; j < 20; j++)
+                    newimage.SetPixel(i, j, clickcolor);
+
+            pictureBox2.Image = newimage;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            Bitmap turgetimage=new Bitmap(pictureBox2.Image);
+            Color pixelcolor=new Color();
+            Color turgetcolor=new Color();
+            turgetcolor=turgetimage.GetPixel(0,0);
+            for(int i = 0 ;i < image.Width;i++)
+                for (int j = 0; j < image.Height; j++)
+                {
+                    pixelcolor = image.GetPixel(i, j);
+                    if(Math.Abs(pixelcolor.R-turgetcolor.R)<int.Parse(textBox1.Text)&&
+                       Math.Abs(pixelcolor.G-turgetcolor.G)<int.Parse(textBox1.Text)&&
+                       Math.Abs(pixelcolor.B-turgetcolor.B)<int.Parse(textBox1.Text))
+                    {
+                        Color clearcolor=new Color();
+                        clearcolor=Color.FromArgb(0,pixelcolor);
+                        image.SetPixel(i, j, clearcolor);
+                    }
+                }
+            pictureBox1.Image = image;
+        }
+        int a=0;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            a++;
+            if (a % 4 == 1)
+                this.BackColor = Color.Red;
+            else if (a % 4 == 2)
+                this.BackColor = Color.Green;
+            else if (a % 4 == 0)
+                this.BackColor = Color.Blue;
+            else if(a%4==3)
+                this.BackColor = System.Drawing.SystemColors.Control;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(pictureBox1.Image);
+            image.Save(@"output.png");
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            using (MemoryStream pngMemStream = new MemoryStream())
+                if (e.KeyCode == Keys.V && e.Control == true)
+            {
+                if(Clipboard.ContainsImage())
+                {
+                    IDataObject iData = Clipboard.GetDataObject();
+                    Bitmap image = (Bitmap)iData.GetData(DataFormats.Bitmap);
+                    pictureBox1.Image = image;
+                    pictureBox1.Size = image.Size;
+                    this.Height = pictureBox1.Size.Height + 200;
+                    this.Width = pictureBox1.Size.Width + 200;
+                    button2.Enabled = true;
+                }
+            }
+            else if (e.KeyCode == Keys.C && e.Control == true)
+            {
+                Clipboard.SetImage(pictureBox1.Image);
+            }
+        }
+
+
+
+    }
+}
